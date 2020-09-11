ad8b583bb53f9562fcf8f1306a1bfca1e06da66f
Froust <abdbeg123@gmail.com>
2020-09-11 22:24:43 +0500

    3

--- Run.cmd
+++
@@ -1,4 +0,0 @@
-cd
-
-npm run serve
-
--- src/Icons.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck, faPlus, faEdit, faMinus, faExclamationTriangle, faSave, faDownload, faUpload, faEye, faTrash, faDotCircle, faHourglass, faTimes } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faCheck, faPlus, faEdit, faMinus, faExclamationTriangle, faSave, faDownload, faUpload, faEye, faTrash, faDotCircle, faHourglass, faTimes);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
